d77a6bbde974f2279eb7b468a3bc02185b9871cd
mathpvx <7547@holbertonstudents.com>
2024-06-08 22:30:42 +0200

    added comments

--- backend/models/quizModel.js
+++ backend/models/quizModel.js
@@ -1,6 +1,10 @@
+// import connection object
 const db = require('../config/db');
 
+// saves quiz answers into the quiz_answers table
 exports.saveAnswers = (answers, callback) => {
+  // insert a new record for answers where ? is a placeholder
   const query = 'INSERT INTO quiz_answers (answers) VALUES (?)';
+  // runs the SQL query defined above 
   db.query(query, [JSON.stringify(answers)], callback);
 };
