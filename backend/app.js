9beef0ea6b2dbfd1c31a0ad78fe446025b7313f7
mathpvx <7547@holbertonstudents.com>
2024-06-08 18:07:13 +0200

    instantiates express server, body parses and quiz routes

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const db = require('./config/db');
+const quizRoutes = require('./routes/quiz');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use('/api/quiz', quizRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
