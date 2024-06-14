a4a978019179e9e964ce3b202bfdf17e43c295ee
mathpvx <7547@holbertonstudents.com>
2024-06-14 17:28:46 +0200

    new associations file to solve circular import pb while linking models

---
+++ backend/models/associations.js
@@ -0,0 +1,18 @@
+const Question = require('./question');
+const Session = require('./session');
+const SessionAnswer = require('./sessionAnswer');
+const AnswerOption = require('./answerOption');
+const Recommendation = require('./recommendation');
+
+// Associations
+AnswerOption.belongsTo(Question, { foreignKey: 'next_qu' });
+AnswerOption.belongsTo(Recommendation, { foreignKey: 'reco_id' });
+
+Question.hasOne(Session, { foreignKey: 'next_qu' });
+Question.hasMany(SessionAnswer, { foreignKey: 'qu_id' });
+
+Session.belongsTo(Question, { foreignKey: 'next_qu' });
+Session.hasMany(SessionAnswer, { foreignKey: 'sess_id' });
+
+SessionAnswer.belongsTo(Session, { foreignKey: 'sess_id' });
+SessionAnswer.belongsTo(AnswerOption, { foreignKey: 'answer_id' });
