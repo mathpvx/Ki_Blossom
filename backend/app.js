b80adb03bdeeb8de1139bb012c9408780746f467
mathpvx <7547@holbertonstudents.com>
2024-06-14 10:07:03 +0200

    add session endpoint

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const sequelize = require('./config/db');
 const quizRoutes = require('./routes/quiz');
+const sessionRoutes = require('./routes/session');
+const cookieParser = require('cookie-parser');
+const sessionMiddleware = require('./middlewares/session');
+const Session = require('./models/session');
 const Question = require('./models/questions');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cookieParser());
 app.use('/api/quiz', quizRoutes);
+app.use('/api/session', sessionRoutes);
 
 // Sync Sequelize models with the database
 // synchronize ORM with DB tables: create them
