5a1851d6ff1045cd6dff930920a93867b0da92bd
mathpvx <7547@holbertonstudents.com>
2024-06-14 17:24:46 +0200

    sessionAnswer model that links session and answer picked

---
+++ backend/models/sessionAnswer.js
@@ -0,0 +1,18 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const SessionAnswer = sequelize.define('SessionAnswer', {
+  sess_id: {
+    type: DataTypes.STRING,
+    primaryKey: true
+  },
+  answer_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true
+  }
+}, {
+  tableName: 'session_answer',
+  timestamps: false
+});
+
+module.exports = SessionAnswer;
