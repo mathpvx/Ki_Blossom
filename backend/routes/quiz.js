f1607394f9a784e865b91a421e9940accb7f11ee
mathpvx <7547@holbertonstudents.com>
2024-06-15 19:21:53 +0200

    get next question instead of random

--- backend/routes/quiz.js
+++ backend/routes/quiz.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const Question = require('../models/question'); 
-const sequelize = require('../config/db'); 
+const Question = require('../models/question');
+const AnswerOption = require('../models/answerOption');
 
 router.get('/questions', async (req, res) => {
   try {
-    // get a random row 
-    const question = await Question.findOne();
-    if (!question) {
-      return res.status(404).json({ error: 'No question found' });
-    }
-    res.json(question); // Match the variable name
+    const questionId = req.query.qu_id || 1;
+    const question = await Question.findOne({ where: { qu_id: questionId } });
+    const answerOptions = await AnswerOption.findAll({ where: { current_qu: questionId } });
+
+    res.json({
+      qu_txt: question.qu_txt,
+      answerOptions: answerOptions.map(option => ({
+        answer_id: option.answer_id,
+        answer_button: option.answer_button,
+      })),
+    });
   } catch (error) {
     console.error('Error fetching question:', error);
     res.status(500).json({ error: 'Server error' });
