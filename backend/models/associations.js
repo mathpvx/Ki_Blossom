60d7175470a21205dc4c8e4a226c58bc0a51ff0c
mathpvx <7547@holbertonstudents.com>
2024-06-19 11:39:54 +0200

    add associations between model categories and reco

--- backend/models/associations.js
+++ backend/models/associations.js
@@ -2,7 +2,13 @@ const Question = require('./question');
 const Session = require('./session');
 const SessionAnswer = require('./sessionAnswer');
 const AnswerOption = require('./answerOption');
-const Recommendation = require('./recommendation.js');
+const Recommendation = require('./recommendation');
+const WbExercice = require('./wbExercice');
+const Unhealthy = require('./unhealthy');
+const Plants = require('./plant');
+const EssentialOil = require('./essentialOil');
+const Dietetics = require('./dietetics');
+const Food = require('./food');
 
 // Associations
 AnswerOption.belongsTo(Question, { foreignKey: 'next_qu' });
@@ -17,3 +23,14 @@ Session.hasMany(SessionAnswer, { foreignKey: 'sess_id' });
 
 SessionAnswer.belongsTo(Session, { foreignKey: 'sess_id' });
 SessionAnswer.belongsTo(AnswerOption, { foreignKey: 'answer_id' });
+
+Recommendation.hasMany(AnswerOption, { foreignKey: 'reco_id' });
+
+Recommendation.belongsTo(AnswerOption, { foreignKey: 'answer_id' });
+Recommendation.belongsTo(Unhealthy, { foreignKey: 'unhealthy_id' });
+Recommendation.belongsTo(WbExercice, { foreignKey: 'exo_id' });
+Recommendation.belongsTo(Food, { as: 'Food1', foreignKey: 'food_id1' });
+Recommendation.belongsTo(Food, { as: 'Food2', foreignKey: 'food_id2' });
+Recommendation.belongsTo(Plants, { foreignKey: 'plant_id' });
+Recommendation.belongsTo(EssentialOil, { foreignKey: 'eo_id' });
+Recommendation.belongsTo(Dietetics, { foreignKey: 'diet_id' });
