d2c550442febe9b7ba29aa91939b2d56a454c152
mathpvx <7547@holbertonstudents.com>
2024-06-19 11:39:25 +0200

    update reco model with columns for each category FK id

--- backend/models/recommendation.js
+++ backend/models/recommendation.js
@@ -6,6 +6,55 @@ const Recommendation = sequelize.define('Recommendation', {
     type: DataTypes.INTEGER,
     primaryKey: true,
     autoIncrement: true
+  },
+  unhealthy_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'unhealthy',
+      key: 'id'
+    }
+  },
+  exo_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'wb_exercice',
+      key: 'id'
+    }
+  },
+  food_id1: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'food',
+      key: 'id'
+    }
+  },
+  food_id2: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'food',
+      key: 'id'
+    }
+  },
+  plant_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'plant',
+      key: 'id'
+    }
+  },
+  eo_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'essential_oil',
+      key: 'id'
+    }
+  },
+  diet_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'dietetics',
+      key: 'id'
+    }
   }
 }, {
   tableName: 'recommendation',
